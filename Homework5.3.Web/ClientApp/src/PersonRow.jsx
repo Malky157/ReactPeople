e9c3558b7d45a4ea6a61f6e6de9159bc212b3ffe
Malky Kohn <malkykohn@outlook.com>
2023-05-08 17:09:40 -0400

    Add project files.

---
+++ Homework5.3.Web/ClientApp/src/PersonRow.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class PersonRow extends React.Component{
+   
+    render(){
+
+        return (<tr className= {this.props.currentPerson.age >= 65 ?'table-danger':""}>
+            <th>{this.props.currentPerson.firstName}</th>
+            <th>{this.props.currentPerson.lastName}</th>
+            <th>{this.props.currentPerson.age}</th>
+        </tr>)
+        
+    }
+}
+export default PersonRow
